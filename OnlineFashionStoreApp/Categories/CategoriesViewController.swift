24a96446912d8321a6866cbb3c07db33ac2368a5
TDB-star <tatiana.blache@gmail.com>
2022-01-14 11:39:25 +0100

    first screen - UICollectionView

--- OnlineFashionStoreApp/Categories/CategoriesViewController.swift
+++ OnlineFashionStoreApp/Categories/CategoriesViewController.swift
@@ -7,14 +7,11 @@
 
 import UIKit
 
-class CategoriesViewController: UIViewController {
+class CategoriesCollectionViewController: UICollectionViewController {
 
-    @IBOutlet weak var collectionView: UICollectionView!
-   
     private let cellIdentifier = "CategoryCell"
     private let segueIdentifier = "ToSabcategoryViewController"
     
-
     private var viewModel: CategoryViewModelProtocol! {
         didSet {
             viewModel.fetchData {
@@ -25,55 +22,29 @@ class CategoriesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        viewModel = CategoryListViewModel()
-        
-        collectionView.collectionViewLayout = createLayout()
-        
 
+        viewModel = CategoryListViewModel()
     }
 }
 
-extension CategoriesViewController: UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+extension CategoriesCollectionViewController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         viewModel.getNumberOfRows()
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CategoryCollectionViewCell
         cell.viewModel = viewModel.getCellViewModel(at: indexPath)
         return cell
     }
 }
 
-extension CategoriesViewController: UICollectionViewDelegate {
-    
-}
-
-extension CategoriesViewController {
-    private func createLayout() -> UICollectionViewCompositionalLayout {
- 
-        let layout = UICollectionViewCompositionalLayout { (sectionIndex: Int, environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
-                        
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(86))
-            
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            
-            let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            
-            let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitems: [item])
-            group.interItemSpacing = .flexible(2)
-            let section = NSCollectionLayoutSection(group: group)
-            section.orthogonalScrollingBehavior = .continuous
-            section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0)
-            
-            return section
-        }
-        let configuration = UICollectionViewCompositionalLayoutConfiguration()
-        layout.configuration = configuration
-        
-        return layout
+extension CategoriesCollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width, height: 100)
     }
 }
+
+
+
 
--- OnlineFashionStoreApp/Categories/CategoryCollectionViewCell.swift
+++ OnlineFashionStoreApp/Categories/CategoryCollectionViewCell.swift
@@ -10,24 +10,18 @@ import UIKit
 class CategoryCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryImageView: CustomImageView!
     
     var viewModel: CategoryCellViewModelProtocol! {
         didSet {
         nameLabel.text = viewModel.categoryName
-
-            //categoryImageView.fetchImage(from: viewModel.imageUrl)
-                       guard let imageData = viewModel.imageData else { return }
-                      categoryImageView.image = UIImage(data: imageData)
+//                       guard let imageData = viewModel.imageData else { return }
+//                      categoryImageView.image = UIImage(data: imageData)
+            categoryImageView.fetchImage(from: viewModel.imageUrl)
             categoryImageView.layer.cornerRadius = categoryImageView.frame.size.width / 2
             categoryImageView.clipsToBounds = true
             categoryImageView.contentMode = .scaleAspectFill
             
         }
     }
-    
-//    override func prepareForReuse() {
-//        super.prepareForReuse()
-//        categoryImageView.image = nil
-//    }
 }
